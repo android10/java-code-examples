c795259e9a42c66b823f7f0ab159346312441ce9
Fernando Cejas <fcejas@gmail.com>
2016-02-19 15:06:45 +0100

    Add use case scenarios for optional examples.

--- src/main/java/com/fernandocejas/java/samples/optional/OptionalMenuOption.java
+++ src/main/java/com/fernandocejas/java/samples/optional/OptionalMenuOption.java
@@ -1,8 +1,29 @@
 package com.fernandocejas.java.samples.optional;
 
+import com.fernandocejas.arrow.annotations.See;
+import java.util.ArrayList;
+import java.util.List;
+
+@See(ref = "")
 public class OptionalMenuOption {
+  private final List<Object> examples;
+
+  public OptionalMenuOption() {
+    examples = new ArrayList<>(3);
+    examples.add(new UseCaseScenario01());
+    examples.add(new UseCaseScenario02());
+    examples.add(new UseCaseScenario03());
+  }
+
   public void run() {
-    System.out.println("Optional Option pressed...");
+    for (Object object : examples) {
+      printMessage(object.toString());
+    }
+    printMessage("Exiting example...");
     System.exit(0);
+  }
+
+  private void printMessage(String message) {
+    System.out.println(message);
   }
 }
---
+++ src/main/java/com/fernandocejas/java/samples/optional/UseCaseScenario02.java
@@ -0,0 +1,12 @@
+package com.fernandocejas.java.samples.optional;
+
+import com.fernandocejas.arrow.annotations.See;
+import com.fernandocejas.arrow.strings.Strings;
+
+@See(ref = "")
+public class UseCaseScenario02 {
+
+  @Override public String toString() {
+    return Strings.EMPTY;
+  }
+}
