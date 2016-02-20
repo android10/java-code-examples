d0f3197cb29906bd3021a614deaa6b5675004dc1
Fernando Cejas <fcejas@gmail.com>
2016-02-20 16:25:03 +0100

    Refactor to use lambdas.

--- src/main/java/com/fernandocejas/java/samples/optional/UseCaseScenario03.java
+++ src/main/java/com/fernandocejas/java/samples/optional/UseCaseScenario03.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import rx.Observable;
-import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
@@ -14,20 +13,11 @@ import rx.schedulers.Schedulers;
 public class UseCaseScenario03 {
 
   public static final Func1<Optional<List<String>>, Observable<List<String>>> TO_AD_ITEM =
-      new Func1<Optional<List<String>>, Observable<List<String>>>() {
-        @Override public Observable<List<String>> call(Optional<List<String>> ads) {
-          return ads.isPresent()
-              ? Observable.just(ads.get())
-              : Observable.just(Collections.<String>emptyList());
-        }
-      };
+      ads -> ads.isPresent()
+          ? Observable.just(ads.get())
+          : Observable.just(Collections.<String>emptyList());
 
-  public static final Func1<List<String>, Boolean> EMPTY_ELEMENTS =
-      new Func1<List<String>, Boolean>() {
-        @Override public Boolean call(List<String> ads) {
-          return !ads.isEmpty();
-        }
-      };
+  public static final Func1<List<String>, Boolean> EMPTY_ELEMENTS = ads -> !ads.isEmpty();
 
   public UseCaseScenario03() {
     //empty
@@ -35,11 +25,9 @@ public class UseCaseScenario03 {
 
   @Override public String toString() {
     final StringBuilder builder = new StringBuilder();
-    feed().subscribe(new Action1<List<String>>() {
-      @Override public void call(List<String> feed) {
-        for (String feedElement : feed) {
-          builder.append("\n").append(feedElement);
-        }
+    feed().subscribe(feed -> {
+      for (String feedElement : feed) {
+        builder.append("\n").append(feedElement);
       }
     });
     return builder.toString();
@@ -53,11 +41,11 @@ public class UseCaseScenario03 {
         .observeOn(Schedulers.immediate());
   }
 
-  private Observable<List<String>> users() {
-    return Observable.just(Arrays.asList("IronMan", "Wolverine", "Batman", "Superman"));
-  }
-
   private Observable<Optional<List<String>>> ads() {
     return Observable.just(Optional.fromNullable(Collections.singletonList("This is and Ad")));
+  }
+
+  private Observable<List<String>> users() {
+    return Observable.just(Arrays.asList("IronMan", "Wolverine", "Batman", "Superman"));
   }
 }
